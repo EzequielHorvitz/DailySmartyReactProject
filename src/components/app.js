4ab9eeb619d30cab82faeb54fe322eabe2948b42
Ezequiel H <horvitzezequiel@gmail.com>
2021-07-20 20:12:12 +0200

    setup logo component and corresponding styles

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          logo
+          <Logo/>
+        </div>
+      </div>
+    );
+  }
+}
